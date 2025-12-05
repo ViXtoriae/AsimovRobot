9ba9e2cbb8e60dbb79caea152fe547bac4fdf373
Lucas SF <lucas.san-frutos@esme.fr>
2025-12-05 16:21:07 +0100

    Changement vue

--- VIEW/vue_priorite_robots.sql
+++ VIEW/vue_priorite_robots.sql
@@ -1,18 +1,34 @@
+ALTER TABLE robots ADD priorite_modele INT;
+
+UPDATE robots SET priorite_modele =
+    CASE modele
+        WHEN 'A100' THEN 2
+        WHEN 'A200' THEN 3
+        WHEN 'B100' THEN 1
+        WHEN 'C300' THEN 1
+        ELSE 1
+    END;
+
+
+
 CREATE OR REPLACE VIEW vue_priorite_robots AS
 SELECT
     r.id_robot,
     r.nom_robot,
     r.modele,
     r.etat,
-    mp.coefficient AS priorite_modele,
+
+    -- Succès comptés simplement
     IFNULL(SUM(CASE WHEN a.resultat = 'succès' THEN 1 ELSE 0 END), 0) AS succes,
+
+    -- Score simplifié
     (
-        (mp.coefficient * 10)
-        + (IFNULL(SUM(CASE WHEN a.resultat = 'succès' THEN 1 ELSE 0 END), 0) * 5)
-        + (CASE WHEN r.etat = 'actif' THEN 50 ELSE 0 END)
+        (r.priorite_modele * 10) +
+        (IFNULL(SUM(CASE WHEN a.resultat = 'succès' THEN 1 ELSE 0 END), 0) * 5) +
+        (CASE WHEN r.etat = 'actif' THEN 50 ELSE 0 END)
     ) AS score_priorite
+
 FROM robots r
-LEFT JOIN actions a ON a.id_robot = r.id_robot
-LEFT JOIN modele_priorite mp ON r.modele = mp.modele
-GROUP BY r.id_robot, r.nom_robot, r.modele, r.etat, mp.coefficient
+LEFT JOIN actions a ON r.id_robot = a.id_robot
+GROUP BY r.id_robot, r.nom_robot, r.modele, r.etat, r.priorite_modele
 ORDER BY score_priorite DESC;
