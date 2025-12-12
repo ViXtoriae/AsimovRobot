e41395e5615e792c3f138c7a635cd375f74ff09d
Vic M <victoria.marteau@esme.fr>
2025-12-12 10:31:21 +0100

    Suppr view

--- 1st PART/TABLES ADDED/robot_modeles_prio.sql
+++ 1st PART/TABLES ADDED/robot_modeles_prio.sql
@@ -3,13 +3,15 @@ create tables robot_modeles_prio (
     priorite_loi INT CHECK (priorite_loi IN (1,2,3))
 );
 
-insert into robot_modeles_prio (modele, priorite_loi) values
-    ('A100', 1),
-    ('A200', 2),
-    ('B100', 3),
-    ('C300', 2),
-    ('TNG1701', 1),
-    ('SWProtocol', 3),
-    ('SWAstro', 1),
-    ('Skynet', 3),
-    ('PixarEVE', 2);
+INSERT INTO robot_modeles_prio (modele, priorite_loi) VALUES
+    ('PokeVolt', 1),        -- Pikachu : priorité Loi 1 (protection des humains)
+    ('HylianBlade', 2),     -- Link : priorité Loi 2 (obéissance aux ordres)
+    ('B100', 3),            -- GammaRay : priorité Loi 3 (préservation de soi)
+    ('C300', 3),            -- DeltaForce : priorité Loi 3
+    ('PokeEvolve', 1),      -- Évoli : priorité Loi 1
+    ('HorizonHunter', 1),   -- Aloy : priorité Loi 1 (combat pour protéger les humains)
+    ('SWProtocol', 2),      -- C3PO : priorité Loi 2 (protocole et obéissance)
+    ('SWAstro', 2),         -- R2D2 : priorité Loi 2 (missions techniques)
+    ('Skynet', 3),          -- T800 : priorité Loi 3 (préservation de soi, logique Skynet)
+    ('PixarEVE', 1);        -- Wall-E : priorité Loi 1 (sauver et protéger)
+
